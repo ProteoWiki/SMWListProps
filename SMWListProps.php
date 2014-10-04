fba9cd1214e1780999530afbe694f2a808258234
Toni Hermoso Pulido <toniher@cau.cat>
2014-10-04 13:41:52 +0200

    Cleaning

--- SMWListProps.php
+++ SMWListProps.php
@@ -10,16 +10,8 @@ if ( !defined( 'SMW_VERSION' ) ) {
 	exit( 1 );
 }
 
-#
-# This is the path to your installation of SemanticTasks as
-# seen from the web. Change it if required ($wgScriptPath is the
-# path to the base directory of your wiki). No final slash.
-# #
-$spScriptPath = $wgScriptPath . '/extensions/SMWListProps';
-#
-
 # Extension credits
-$wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other'][] = array(
+$GLOBALS['wgExtensionCredits']['semantic'][] = array(
 	'path' => __FILE__,
 	'name' => 'SMWListProps',
 	'author' => array(
@@ -32,16 +24,17 @@ $wgExtensionCredits[defined( 'SEMANTIC_EXTENSION_TYPE' ) ? 'semantic' : 'other']
 
 
 // i18n
-$wgExtensionMessagesFiles['SMWListProps'] = dirname( __FILE__ ) . '/SMWListProps.i18n.php';
-$wgExtensionMessagesFiles['SMWListPropsMagic'] = dirname( __FILE__ ) . '/SMWListProps.i18n.magic.php';
+$GLOBALS['wgMessagesDirs']['SMWListProps'] = dirname( __FILE__ ) . 'i18n';
+$GLOBALS['wgExtensionMessagesFiles']['SMWListProps'] = dirname( __FILE__ ) . '/SMWListProps.i18n.php';
+$GLOBALS['wgExtensionMessagesFiles']['SMWListPropsMagic'] = dirname( __FILE__ ) . '/SMWListProps.i18n.magic.php';
 
 // Autoloading
-$wgAutoloadClasses['SMWListProps'] = dirname( __FILE__ ) . '/SMWListProps.classes.php';
+$GLOBALS['wgAutoloadClasses']['SMWListProps'] = dirname( __FILE__ ) . '/SMWListProps.classes.php';
 
 $wgHooks['ParserFirstCallInit'][] = 'wfRegisterSMWListProps';
+
+
 // Hooks
-
-
 function wfRegisterSMWListProps( $parser ) {
 
 	$parser->setFunctionHook( 'SMWListProps', 'SMWListProps::executeGetListProps', SFH_OBJECT_ARGS );
